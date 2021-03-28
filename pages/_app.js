f0d45d0676d06da5285cd2b92c9c0dcfcacbfc1c
Darren Waddell <darren@stepzen.com>
2021-03-28 19:45:58 +0100

    First commit

---
+++ pages/_app.js
@@ -0,0 +1,21 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+
+import '../styles/globals.css'
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  headers: {
+    Authorization: `Apikey ${process.env.NEXT_PUBLIC_STEPZEN_API_KEY}`,
+  },
+  uri: process.env.NEXT_PUBLIC_STEPZEN_URI
+});
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
+}
+
+export default MyApp
